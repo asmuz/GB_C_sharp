2d30c762f5f8e6ff11a31e382dbccd858a9a3c6d
AsmuZ <asmuz@mail.ru>
2023-08-06 02:00:30 +0300

    Домашняя работа. Задача 8

---
+++ task08/Program.cs
@@ -0,0 +1,15 @@
+﻿/*
+Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+5 -> 2, 4
+8 -> 2, 4, 6, 8
+*/
+
+Console.Clear();
+
+Console.WriteLine("Введите целое число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 2; i < number; i=i+2)
+{
+    Console.Write(i + " ");
+}
