00befe4d35c0e4aa0e5c3834ac6b783356a9d807
AsmuZ <asmuz@mail.ru>
2023-08-06 02:22:16 +0300

    Семинар 2. Задача 12

---
+++ task12/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+
+Console.WriteLine("Введите первое число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+int result = number1 % number2;
+
+if (result == 0) Console.WriteLine("Кратно");
+else Console.WriteLine("некратно, остаток " + result);
