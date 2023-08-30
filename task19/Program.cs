1cf2d17bbb2a07f9fe4fdead4488c173eabcadbf
AsmuZ <asmuz@mail.ru>
2023-08-30 23:32:11 +0300

    Домашка 3 урок

---
+++ task19/Program.cs
@@ -0,0 +1,37 @@
+﻿/* Задача 19
+Напишите программу, которая принимает на вход пятизначное (или ЛЮБОЕ ) число и проверяет, является ли оно палиндромом.
+14212 -> нет
+12821 -> да
+23432 -> да
+*/
+
+Console.Clear();
+
+Console.WriteLine("Введите ЛЮБОЕ число: ");
+int inputNumber = Convert.ToInt32(Console.ReadLine());
+int number = inputNumber;//делаем копию числа, с которой будем работать
+int reverseNumber = 0;
+
+// создаем "множитель" для дальнейшего "разворота числа"
+int countNumber = inputNumber;//делаем копию исходного числа, для получения разрядности
+int multiplier = 1;//множитель
+while (countNumber > 0) {
+    multiplier *= 10;
+    countNumber /= 10;
+}
+multiplier /= 10;//корректировка итогового множителя (нужно на 1 разряд мемньше)
+    
+while (number > 0) {
+    int lastNumber = number % 10;
+    reverseNumber += lastNumber * multiplier;
+    number /= 10;
+    multiplier /= 10;
+}
+
+if (reverseNumber == inputNumber) {
+    Console.WriteLine("Круто! " + inputNumber + " это число ПОЛИНДРОМ!");
+}
+
+else {
+    Console.WriteLine("Число " + inputNumber + " и его развёрнутая версия " + reverseNumber + " отличаются");
+}
---
+++ task21/Program.cs
@@ -0,0 +1,21 @@
+﻿/* Задача 21
+Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+A (3,6,8); B (2,1,-7), -> 15.84
+A (7,-5, 0); B (1,-1,9) -> 11.53
+*/
+
+Console.Clear();
+
+Console.WriteLine("Введите координаты ПЕРВОГО числа (X1, Y1, Z1): ");
+int X1 = Convert.ToInt32(Console.ReadLine());
+int Y1 = Convert.ToInt32(Console.ReadLine());
+int Z1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите координаты ВТОРОГО числа (X2, Y2, Z2): ");
+int X2 = Convert.ToInt32(Console.ReadLine());
+int Y2 = Convert.ToInt32(Console.ReadLine());
+int Z2 = Convert.ToInt32(Console.ReadLine());
+
+double distance = Math.Sqrt(Math.Pow(X1 - X2, 2) + Math.Pow(Y1 - Y2, 2) + Math.Pow(Z1 - Z2, 2));
+
+Console.WriteLine("Расстояние между точками: " + Math.Round(distance, 2));
