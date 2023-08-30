e9613d70f5d78803afdc8cbbca41c8677fd4e78d
AsmuZ <asmuz@mail.ru>
2023-08-31 00:17:04 +0300

    Домашка. Задача 25

---
+++ task25/Program.cs
@@ -0,0 +1,22 @@
+﻿/*Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+3, 5 -> 243 (3⁵)
+2, 4 -> 16
+*/
+
+int ExponentOfNumber(int number, int exponent){
+    int result = 1;
+    for (int i = 0; i < exponent; i++) {
+        result = result * number;
+    }
+    return result;
+}
+
+Console.Clear();
+
+Console.Write("Какое число возводим? ");
+int userNumber = Convert.ToInt32(Console.ReadLine());
+Console.Write("В какую степень? ");
+int userExponent = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"Результатом возведения числа {userNumber} в степень {userExponent} является число "
++ ExponentOfNumber(userNumber, userExponent));
