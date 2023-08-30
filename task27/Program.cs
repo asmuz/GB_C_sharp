7776b15e93632cd02ef920782cbf6f3e2225e4b1
AsmuZ <asmuz@mail.ru>
2023-08-31 00:37:38 +0300

    Домашка. Задача 27

---
+++ task27/Program.cs
@@ -0,0 +1,21 @@
+﻿/*Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12
+*/
+
+int SumOfDigits(int number){
+    int result = 0;
+    while(number > 0){
+        result += number % 10;
+        number /= 10;
+    }
+    return result;
+}
+
+Console.Clear();
+
+Console.Write("Введите любое число: ");
+int userNumber = Convert.ToInt32(Console.ReadLine());
+int sum = SumOfDigits(userNumber);
+Console.WriteLine($"Сумма всех цифр в числе {userNumber} равна {sum}");
