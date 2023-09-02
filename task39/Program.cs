dba20fa25dc5516dd2fdab11a97237c02d6907f0
AsmuZ <asmuz@mail.ru>
2023-09-02 15:54:12 +0300

    Task 39

---
+++ task39/Program.cs
@@ -0,0 +1,45 @@
+﻿/* Напишите программу, которая перевернёт одномерный массив (последний элемент будет на первом месте, а первый - на последнем и т.д.)
+[1 2 3 4 5] -> [5 4 3 2 1]
+[6 7 3 6] -> [6 3 7 6]
+Комментарий: эту задачу можно решить 2 способами:
+1) менять числа местами в исходном массиве;
+2) создать новый массив и в него записать перевёрнутый исходный массив по элементам.*/
+
+Console.Clear();
+
+int[] GetArray(int length, int min, int max)
+{
+    int[] array = new int[length];
+    for (int i = 0; i < length; i++)
+    {
+        array[i] = new Random().Next(min, max);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i]);
+        if (i < array.Length - 1) Console.Write(", ");
+    }
+    Console.Write("]");
+}
+
+int[] ReverseArray(int[] array)
+{
+    int[] result = new int[array.Length];
+    for (int i = 0; i < array.Length; i++)
+    {
+        result[i] = array[array.Length - i - 1];
+    }
+    return result;
+}
+
+int[] sourceArray = GetArray(10, 0, 10);
+PrintArray(sourceArray);
+Console.Write(" -> ");
+int[] reverseArray = ReverseArray(sourceArray);
+PrintArray(reverseArray);
