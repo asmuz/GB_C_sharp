2ea5481fcf370651cf1158108e8d6c3936f4dce2
AsmuZ <asmuz@mail.ru>
2023-09-02 16:57:53 +0300

    Task 42

---
+++ task42/Program.cs
@@ -0,0 +1,24 @@
+﻿/* Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+45 -> 101101
+3 -> 11
+2 -> 10
+*/
+
+Console.Clear();
+
+Console.Write("Введите десятичное число: ");
+int dexNumber = Convert.ToInt32(Console.ReadLine());
+
+string ToBinary(int number)
+{
+    string result = String.Empty;
+    int temp = 0;
+    for (int i = number; i > 0; i /= 2)
+    {
+        temp = i % 2;
+        result = temp + result;
+    }
+    return result;
+}
+
+Console.WriteLine(ToBinary(dexNumber));
