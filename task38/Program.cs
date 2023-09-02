359b2302b413f4f88713c45bd97558ee4ee52d26
AsmuZ <asmuz@mail.ru>
2023-09-02 14:12:15 +0300

    Task 38

---
+++ task38/Program.cs
@@ -0,0 +1,53 @@
+﻿/* Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+[3.22, 4.2, 1.15, 77.15, 65.2] => 77.15 - 1.15 = 76
+*/
+
+Console.Clear();
+
+double[] GetDoubleArray(int length, int min, int max)
+{
+    double[] array = new double[length];
+    for (int i = 0; i < length; i++)
+    {
+        //array[i] = new Random().NextDouble(min, max);
+        array[i] = Math.Round((new Random().NextDouble() * (max - min) + min), 2);
+    }
+    return array;
+}
+
+void PrintArray(double[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i]);
+        if (i < array.Length - 1) Console.Write(", ");
+    }
+    Console.Write("]");
+}
+
+double GetMax(double[] array)
+{
+    double max = array[0];
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > max) max = array[i];
+    }
+    return max;
+}
+
+double GetMin(double[] array)
+{
+    double min = array[0];
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] < min) min = array[i];
+    }
+    return min;
+}
+
+double[] arr = GetDoubleArray(5, 0, 100);
+double max = GetMax(arr);
+double min = GetMin(arr);
+PrintArray(arr);
+Console.Write($" -> {max} - {min} = {max - min}");
