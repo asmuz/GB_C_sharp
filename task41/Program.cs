46316e4d9646fff0779eb53a0bf83cfc6d355cf3
AsmuZ <asmuz@mail.ru>
2023-09-02 17:53:34 +0300

    Task 41

---
+++ task41/Program.cs
@@ -0,0 +1,44 @@
+﻿/* Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+0, 7, 8, -2, -2 -> 2
+1, -7, 567, 89, 223-> 3
+*/
+
+Console.Clear();
+
+int[] array = MyArray();
+PrintArray(array);
+Console.Write(" -> " + PositiveCount(array));
+
+int[] MyArray()
+{
+    Console.Write("Укажите длинну массива: ");
+    int length = Convert.ToInt32(Console.ReadLine());
+    int[] array = new int[length];
+    for (int i = 0; i < length; i++)
+    {
+        Console.Write($"Элемент {i} из {length - 1}: ");
+        array[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i]);
+        if (i < array.Length - 1) Console.Write(", ");
+    }
+    Console.Write("]");
+}
+
+int PositiveCount(int[] array)
+{
+    int result = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(array[i] > 0) result++;
+    }
+    return result;
+}
