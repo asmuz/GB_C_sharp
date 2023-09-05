e5990b633f686e685992d5b5b4360f8cddcd2913
AsmuZ <asmuz@mail.ru>
2023-09-05 12:05:44 +0300

    ДЗ 47

---
+++ task47/Program.cs
@@ -0,0 +1,43 @@
+﻿/* Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+m = 3, n = 4.
+0,5 7 -2 -0,2
+1 -3,3 8 -9,9
+8 7,8 -7,1 9
+*/
+
+Console.Clear();
+
+double[,] GetDouble2DArray(int row, int col, int min, int max)
+{
+    double[,] array = new double[row, col];
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < col; j++)
+        {
+            array[i, j] = Math.Round((new Random().NextDouble() * (max - min) + min), 1);
+        }
+    }
+    return array;
+}
+
+void PrintDouble2DArray(double[,] array)
+{
+    System.Console.Write($"[ ]\t");
+    for (int i = 0; i < array.GetLength(1); i++)
+    {
+        Console.Write($"[{i}]\t");
+    }
+    System.Console.WriteLine();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        Console.Write($"[{i}]\t");
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            System.Console.Write(array[i, j] + "\t");
+        }
+        System.Console.WriteLine();
+    }
+}
+
+double[,] arr = GetDouble2DArray(3, 4, -10, 10);
+PrintDouble2DArray(arr);
